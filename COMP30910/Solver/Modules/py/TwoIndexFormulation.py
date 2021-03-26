f748f4d9efa9b7c1aad5c4ab23cd6f7035d4a7cf
Braddy Yeoh <yeohbraddy@gmail.com>
2021-03-26 20:30:13 +0000

    function to run in script

---
+++ COMP30910/Solver/Modules/py/TwoIndexFormulation.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import FileParser as fp
+import PathFinder as pf
+import Solver as solver
+import re
+import os
+import sys
+from linecache import getline
+from gurobipy import Model, GRB, quicksum
+from pathlib import Path
+
+
+
+# In[2]:
+
+
+# In[3]:
+
+
+def solve(is_pruned):
+    path = os.getcwd() + "\COMP30910\Solver\Instances\Instances - Prune/"
+    instances = fp.get_instances(path)
+
+    for instance in instances:
+        print("INSTANCE: ", instance)
+        path += instance
+        lp_relaxation, reduced_cost, active_arcs, cost = solver.solve(
+            path, is_pruned=is_pruned)
+        file_name = instance[:len(instance) - 4] + "-solution.txt"
+        pf.create_solution_file(active_arcs, cost, file_name)
+        print()
